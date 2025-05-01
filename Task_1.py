4b06895b2708b7b3519db672f4f11fd0246cc993
tszone5 <shivam6203723@gmail.com>
2025-05-01 18:48:58 +0530

    Create Task_1.py

---
+++ Task_1.py
@@ -0,0 +1,10 @@
+# Task 1: About Me Program
+
+def about_me():
+    name = input("Enter your name: ")
+    age = input("Enter your age: ")
+    college = input("Enter your college: ")
+    interest = input("Enter your interest: ")
+    
+    print(f"\nHi, I'm {name}, a {age}-year-old from {location}. I am passionate about {interest}!")
+
