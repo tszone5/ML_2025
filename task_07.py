73f1a0c7139810208968d30741e31cce0d499ed3
tszone5 <shivam6203723@gmail.com>
2025-05-06 11:00:28 +0530

    Create task_07.py

---
+++ task_07.py
@@ -0,0 +1,9 @@
+# Student data as tuples
+student1 = (1, "Amit", 18)
+student2 = (2, "Priya", 19)
+student3 = (3, "Rahul", 17)
+
+# Display info
+print("Student 1:", student1)
+print("Student 2:", student2)
+print("Student 3:", student3)
