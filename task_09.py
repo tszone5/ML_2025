bc6777ca860647b49e708a568f8b5bf68caa649d
tszone5 <shivam6203723@gmail.com>
2025-05-06 11:02:01 +0530

    Create task_09.py

---
+++ task_09.py
@@ -0,0 +1,9 @@
+# Two sets
+football = {"Amit", "Rahul", "Sneha"}
+cricket = {"Rahul", "Karan", "Sneha"}
+
+# Both games
+print("Both:", football & cricket)
+
+# Only one game
+print("Only one:", football ^ cricket)
